2e97a9c5c3cb3a12a9af53e23f1f4ec190b0c857
Vitaliy Volynskiy <i@vitalya.me>
2025-11-17 22:27:48 +0500

    fix(flake): default script value

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  # TODO: tidy up
   description = "devon-service";
 
   inputs = {
@@ -7,45 +8,66 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+    let
+      devonServerPackage = system:
         let
           pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          packages.default = pkgs.stdenv.mkDerivation {
-            pname = "devon-server";
-            version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
+          yarnBerry = pkgs.yarn-berry;
+          pkgJson = builtins.fromJSON (builtins.readFile ./package.json);
+          offlineCache = yarnBerry.fetchYarnBerryDeps {
             src = ./.;
-            buildInputs = [ pkgs.nodejs pkgs.yarn-berry ];
-            installPhase = ''
-              mkdir -p $out/app
-              cp -r * $out/app
-            '';
-            buildPhase = ''
-              yarn install --immutable
-              yarn build
-            '';
+            lockFile = ./yarn.lock;
+            missingHashes = ./missing-hashes.json;
+            hash = "sha256-Kl81/hJFzNKcs+0jfeO8vQcb2yRt3e2QUSpmVq9+9Os=";
           };
-        }) // {
-      nixosModules.devon-server = { config, pkgs, lib, ... }:
-        let
-          cfg = config.services.devon-server;
         in
-        {
+        pkgs.stdenv.mkDerivation {
+          pname = "devon-server";
+          version = pkgJson.version;
+          src = ./.;
+
+          buildInputs = [ pkgs.nodejs yarnBerry yarnBerry.yarnBerryConfigHook ];
+          nativeBuildInputs = [ pkgs.nodejs yarnBerry.yarnBerryConfigHook ];
+          NODE_PATH = offlineCache;
+          missingHashes = ./missing-hashes.json;
+          offlineCache = offlineCache;
+
+          env = {
+            HOME = "/build";
+            YARN_ENABLE_GLOBAL_CACHE = "false";
+            YARN_ENABLE_TELEMETRY = "0";
+          };
+
+          buildPhase = ''
+            yarn install --immutable
+            yarn build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/app
+            cp -r * $out/app
+          '';
+        };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system: {
+        packages.default = devonServerPackage system;
+      }) // {
+      nixosModules.devon-server = { config, pkgs, lib, ... }:
+        let cfg = config.services.devon-server; in {
           options.services.devon-server = {
             enable = lib.mkEnableOption "Enable devon-server service";
             package = lib.mkOption {
               type = lib.types.package;
-              default = self.packages.${config.nixpkgs.system}.default;
+              default = devonServerPackage pkgs.system;
+            };
+            script = lib.mkOption {
+              type = lib.types.str;
+              default = "dist/main.js";
             };
             envFile = lib.mkOption {
               type = lib.types.path;
               default = null;
-            };
-            script = lib.mkOption {
-              type = lib.types.str;
-              example = "dist/main.js";
             };
           };
 
@@ -55,10 +77,10 @@
               after = [ "network.target" ];
               wantedBy = [ "multi-user.target" ];
               serviceConfig = {
-                EnvironmentFile = lib.mkIf (cfg.envFile != null) cfg.envFile;
                 ExecStart = "${pkgs.nodejs}/bin/node ${cfg.package}/app/${cfg.script}";
                 Restart = "always";
-                WorkingDirectory = cfg.package + "/app";
+                WorkingDirectory = "${cfg.package}/app";
+                EnvironmentFile = lib.mkIf (cfg.envFile != null) cfg.envFile;
               };
             };
           };
